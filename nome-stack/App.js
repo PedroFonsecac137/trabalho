679a53e323ff7addbe700063067b5eeef8bd4556
PedroFonsecac137 <128255994+PedroFonsecac137@users.noreply.github.com>
2025-09-25 23:48:16 +0000

    trabalhothiagao

---
+++ nome-stack/App.js
@@ -0,0 +1,55 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 22 }}>🏠 Tela Home</Text>
+      <Button
+        title="Ir para Detalhes"
+        onPress={() => navigation.navigate('Detalhes')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 22 }}>📄 Tela Detalhes</Text>
+      <Button
+        title="Ir para Perfil"
+        onPress={() => navigation.navigate('Perfil')}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 22 }}>👤 Tela Perfil</Text>
+      <Button
+        title="Voltar para Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} />
+        <Stack.Screen name="Perfil" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
